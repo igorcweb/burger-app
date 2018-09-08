05c218a4ac5357b59a8fb94ba7a0c27201efba08
Igor Chernyshev <igorchern07@gmail.com>
2018-09-08 04:21:52 -0500

    layout

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ("Big Kahuna Burger"), ("In-N-Out Burger"),("Whataburger");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Small Kahuna Burger", true), ("Five Guys Burger", true), ('Smash Burger', true);
