895f4f16bed25e65a8644f5b4e787fcc564822d4
Igor Chernyshev <igorchern07@gmail.com>
2018-09-09 23:43:50 -0500

    seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,1 @@
-INSERT INTO burgers (burger_name) VALUES ("Big Kahuna Burger"), ("Royale with Cheese"),("Le Big Mac"), ("Small Kahuna Burger"), ("Five Guys Burger"), ("Smash Burger");
+INSERT INTO burgers (burger_name) VALUES ("Big Kahuna Burger"), ("Royale with Cheese"),("Le Big Mac");
